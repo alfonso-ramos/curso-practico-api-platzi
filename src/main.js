729ecfc4eaac2c3f334dab945160bf1a9909f8e2
Alfonso <alfonsoramos.1803@gmail.com>
2022-06-08 16:31:28 -0600

    arreglado el error de datos duplicados

--- src/main.js
+++ src/main.js
@@ -13,10 +13,10 @@ async function getTrendingMoviesPreview() {
   const movies = data.results;
 
   // console.log({data, movies})
+
+  trendingMoviesPreviewList.innerHTML = ''
+  
   movies.forEach((movie) => {
-    const trendingMoviesPreviewList = document.querySelector(
-      "#trendingPreview .trendingPreview-movieList"
-    );
 
     const movieContainer = document.createElement("div");
     movieContainer.classList.add("movie-container");
@@ -38,10 +38,10 @@ async function getCategoriesPreview() {
   const { data } = await api("/genre/movie/list");
 
   const categories = data.genres;
+  
+  categoriesPreviewList.innerHTML = ''
+
   categories.forEach((category) => {
-    const categoriesPreviewList = document.querySelector(
-      "#categoriesPreview .categoriesPreview-list"
-    );
 
     const categoryContainer = document.createElement("div");
     categoryContainer.classList.add("category-container");
